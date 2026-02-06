b142fe65b589c032a72455c24aab6f7afaebdd0b
marawilliams <mara.williams005@gmail.com>
2026-02-06 08:57:40 -0500

    changes to pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,8 @@
 
  const Home: React.FC = () => (
    <div>
-     <img className="absolute top-[60vh] left-[49vw] -translate-x-1/2 -translate-y-1/2 w-[40vw]" src="/crate.png" alt="crate"/>
-     <img className="absolute top-[60vh] left-[49vw] -translate-x-1/2 -translate-y-1/2 w-[40vw]" src="/crate_front.png" alt="crate"/>
+     <img className="absolute top-[60vh] left-[48.5vw] -translate-x-1/2 -translate-y-1/2 w-[40vw]" src="/crate.png" alt="crate"/>
+     <img className="absolute top-[60vh] left-[48.5vw] -translate-x-1/2 -translate-y-1/2 w-[40vw]" src="/crate_front.png" alt="crate"/>
 
    </div>
  );
